7a18b441fa5424ca1de1a39ab2f7e2d5fdacbc38
coding_gandalf <d.schmidt@reinhausen.com>
2022-06-14 01:54:21 +0200

    Implement a Admin Board  #20

--- src/pages/Analytics.svelte
+++ src/pages/Analytics.svelte
@@ -2,13 +2,23 @@
   import NavigatorBar from "../components/pageComponent/NavigatorBar.svelte";
   import Dashboard from "../components/pageComponent/Dashboard.svelte";
   import Footer from "../components/pageComponent/Footer.svelte";
-  import {getContext} from "svelte";
+  import {getContext, onMount} from "svelte";
+  import { get } from "svelte/store";
+  import { user } from "../stores.js";
+import AdminComponent from "../components/pageComponent/AdminComponent.svelte";
 
-  // const asteoService = getContext("AsteoService");
+  const asteoService = getContext("AsteoService");
+  let userLevel = 0;
   // donationService.logout();
+  onMount(async () =>{
+    userLevel = (await asteoService.getUser(get(user)._id)).rank;
+  })
 </script>
 
 
 <NavigatorBar title="Analytics"/>
 <Dashboard/>
-<Footer/>
+{#if userLevel > 0}
+  <AdminComponent/>
+{/if}
+<Footer/>v
