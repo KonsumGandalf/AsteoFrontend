c988213922ed08ccf9d63f551d1f2d5a3d21f3b5
coding_gandalf <d.schmidt@reinhausen.com>
2022-06-13 17:52:46 +0200

    Analytic Page  #10

--- src/components/subComponent/IconHorizontal.svelte
+++ src/components/subComponent/IconHorizontal.svelte
@@ -3,16 +3,34 @@
     export let iconName = "";
     export let headerText = "";
     export let valueText = ""
+
+    let count = 0; 
+    const updateCount = () => {
+      const target = +valueText;
+      const inc = target/4;
+      if (count < target){
+        count += inc;
+        setTimeout(updateCount, 300);
+      } else {
+        count = target;
+      }
+    }
+    updateCount();
+
 </script>
 
 <div class="p-4">
     <div class="flex flex-row justify-between font-nova w-64 h-32 py-5 px-2 border-b-2 transition duration-300 {color} ">
-        <div class="rounded-full w-16 h-16 bg-maximumBluePurple items-center">
-            <div class="material-icons-outlined text-pastelBlackEerie">{iconName}</div>
+        <div class="flex items-center rounded-full w-16 h-16 bg-maximumBluePurple">
+            <div class="mx-auto material-icons-outlined text-pastelBlackEerie">{iconName}</div>
         </div>
         <div class="flex flex-col justify-center items-center space-y-2">
             <div class="text-sm text-center">{headerText}</div>
+            {#if Number.isInteger(valueText)}
+              <div id="counter" class="counter text-lg text-center text-pastelBlackEerie">{count}</div>
+            {:else}
             <div class="text-lg text-center text-pastelBlackEerie">{valueText}</div>
+            {/if}
         </div>
     </div>
 </div>
